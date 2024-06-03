5c92fcddacda4746661845b1440fac9599f405d8
Maarten van Gompel <proycon@anaproy.nl>
2024-06-03 13:14:46 +0200

    added a richer command-line interface, stdin support, and documentation

---
+++ README.md
@@ -0,0 +1,39 @@
+# Lingua-cli
+
+This is a small command-line tool for language detection, it is a simple
+wrapper around the [lingua-rs](https://github.com/pemistahl/lingua-rs/) library
+for Rust.
+
+## Installation
+
+Ensure you have Rust's package manager `cargo`, then download, isntall and compile `lingua-cli` in one go as follows:
+
+``$ cargo install lingua-cli``
+
+## Usage
+
+Pass text as parameter
+
+``$ lingua-cli bonjour à tous``
+
+Pass text via standard input:
+
+``$ echo "bonjour à tous" | lingua-cli``
+
+Constrain the languages you want to detect using `-l` with iso-639-1 languages
+codes. Constraining the list improves accuracy. Do `-L` to see a list of
+supported languages.
+
+``$ echo "bonjour à tous" | lingua-cli -l "fr,de,es,nl,en"``
+
+To classify input line-by-line, pass ``-n``.
+
+``$ echo -e "bonjour à tous\nhola a todos\nhallo allemaal" | lingua-cli -n -l "fr,de,es,nl,en"``
+
+```
+fr      0.9069164472389637      bonjour à tous
+es      0.918273871035807       hola a todos
+nl      0.988293648761749       hallo allemaal
+```
+
+Output is TSV and consists of an iso-639-1 language code, confidence score, and in line-by-line mode, a copy of the line.
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,108 @@
-use lingua::Language::{
-    Arabic, Chinese, Czech, French, German, Polish, Portuguese, Romanian, Russian, Spanish,
-    Swedish, Ukrainian,
-};
-use lingua::{LanguageDetector, LanguageDetectorBuilder};
-use std::env;
+use clap::Parser;
+use lingua::{IsoCode639_1, Language, LanguageDetector, LanguageDetectorBuilder};
+use std::io::{self, BufRead, Read};
+use std::str::FromStr;
+
+#[derive(Parser)]
+#[command(version, about)]
+struct Args {
+    /// List of iso-639-1 language codes
+    #[arg(
+        short,
+        long,
+        help = "comma seperated list of iso-639-1 codes of languages to detect, if not specified, all supported language will be used.",
+        num_args = 1,
+        value_delimiter = ',',
+        required = false
+    )]
+    languages: Vec<String>,
+
+    #[arg(
+        short = 'n',
+        long,
+        help = "Classify language per line, this only works if text is not supplied directly as an argument"
+    )]
+    per_line: bool,
+
+    #[arg(short = 'L', help = "List supported languages")]
+    list: bool,
+
+    #[arg(short, long, default_value = "\t")]
+    delimiter: String,
+
+    #[arg(required = false)]
+    text: Vec<String>,
+}
 
 fn main() {
+    let args = Args::parse();
+    if args.list {
+        let mut languages: Vec<Language> = Language::all().into_iter().collect();
+        languages.sort();
+        for language in languages {
+            println!("{} - {}", language.iso_code_639_1(), language);
+        }
+        std::process::exit(0);
+    }
+
     // Select languages by ISO 639-1 code.
-    let languages = vec![
-        French, German, Spanish, Portuguese, Russian, Swedish, Chinese, Polish, Czech, Romanian,
-        Arabic, Ukrainian,
-    ];
-    let detector: LanguageDetector = LanguageDetectorBuilder::from_languages(&languages).build();
-    let text: String = env::args()
-        .map(|x| x.to_string())
-        .collect::<Vec<String>>()
-        .join(" ");
-    let results = detector.compute_language_confidence_values(&text);
-    if !results.is_empty() {
-        print!("{} {}\n", results[0].0.iso_code_639_1(), results[0].1);
+    let languages: Vec<_> = args
+        .languages
+        .iter()
+        .map(|lang| {
+            IsoCode639_1::from_str(lang.as_str())
+                .expect("Supported iso639-1 language code expected")
+        })
+        .collect();
+    let detector: LanguageDetector = if languages.is_empty() {
+        LanguageDetectorBuilder::from_all_languages().build()
+    } else {
+        LanguageDetectorBuilder::from_iso_codes_639_1(&languages).build()
+    };
+
+    if !args.text.is_empty() {
+        //text provided as arguments
+        let text: String = args.text.join(" ");
+        let results = detector.compute_language_confidence_values(text);
+        if !results.is_empty() {
+            print!(
+                "{}{}{}\n",
+                results[0].0.iso_code_639_1(),
+                args.delimiter,
+                results[0].1
+            );
+        }
+    } else if args.per_line {
+        let stdin = io::stdin();
+        for line in stdin.lock().lines() {
+            if let Ok(line) = line {
+                let results = detector.compute_language_confidence_values(&line);
+                if !results.is_empty() {
+                    print!(
+                        "{}{}{}{}{}\n",
+                        results[0].0.iso_code_639_1(),
+                        args.delimiter,
+                        results[0].1,
+                        args.delimiter,
+                        line
+                    );
+                }
+            }
+        }
+    } else {
+        let mut buf: Vec<u8> = Vec::new();
+        io::stdin()
+            .read_to_end(&mut buf)
+            .expect("expected input via stdin");
+        let text = String::from_utf8(buf).expect("Input should be valid utf-8");
+        let results = detector.compute_language_confidence_values(text);
+        if !results.is_empty() {
+            print!(
+                "{}{}{}\n",
+                results[0].0.iso_code_639_1(),
+                args.delimiter,
+                results[0].1
+            );
+        }
     }
 }
